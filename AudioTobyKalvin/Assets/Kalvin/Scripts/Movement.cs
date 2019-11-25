ee76ab69b281a23acb7f27b0cf46283fd5f89193
KalvinMalloch <km230683@falmouth.ac.uk>
2019-11-25 23:46:36 +0000

    Created a test scene and implemented footstep sounds.

--- AudioTobyKalvin/Assets/Kalvin/Scripts/Movement.cs
+++ AudioTobyKalvin/Assets/Kalvin/Scripts/Movement.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+	private Rigidbody2D rig;
+	public float speed;
+	public float frequency;
+	public bool walkSound;
+	private AudioSource audioSource;
+    private AudioClip outAudioClip;
+	
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D> ();
+		audioSource = GetComponent<AudioSource>();
+		walkSound = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+	
+	void Move()
+	{
+		float MoveH = Input.GetAxis ("Horizontal");
+		float MoveV = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (MoveH, MoveV);
+		rig.velocity = movement * speed;
+		if ((Input.GetKey("a")) || (Input.GetKey("w")) || (Input.GetKey("s")) || (Input.GetKey("d")))
+        {
+			StartCoroutine(FootSoundDelay());
+        }
+	}
+	
+	private AudioClip CreateMovementAudioClip(float frequency)
+    {
+        int sampleRate = 44100;
+        int sampleLength = 10000;
+        float maxValue = 1f / 4f;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+	
+	IEnumerator FootSoundDelay()
+    {
+		if (walkSound == true) 
+		{
+			walkSound = false;
+			frequency = Random.Range(100, 200);
+			outAudioClip = CreateMovementAudioClip(frequency);
+            audioSource.PlayOneShot(outAudioClip);
+			yield return new WaitForSeconds(0.3f);
+			walkSound = true;
+		}
     }
 }
---
+++ AudioTobyKalvin/Assets/Kalvin/Scripts/PlayerPickup.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+	private AudioSource audioSource;
+    private AudioClip outAudioClip;
+	
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+	
+	private AudioClip CreatePickupAudioClip(float frequency)
+    {
+        int sampleRate = 44100;
+        int sampleLength = 10000;
+        float maxValue = 1f / 4f;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+			frequency = 700;
+			
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.name == "Health") 
+		{
+			print ("test");
+		}
+		
+	}
+}
